2035bb99927d665b48dc0f59c505da4bc84f14c9
Jay Patel <200426940@student.georgianc.on.ca>
2020-02-06 16:55:46 -0500

    completed comp2084-lab4

---
+++ Models/Manufacturer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BellMobiles.Models
+{
+    public class Manufacturer
+    {
+        public Manufacturer()
+        {
+
+            ManufacturerName = "";
+
+            Founded = new DateTime();
+        }
+        [Key]
+        public int ManufacturerID { get; set; }
+        public String ManufacturerName { get; set; }
+
+        public String URL { get; set; }
+        public ICollection<Phone> PhoneList { get; set; }
+        public DateTime Founded { get; set; }
+    }
+}
---
+++ Models/Phone.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BellMobiles.Models
+{
+    public class Phone
+    {
+        public Phone()
+        {
+            
+            PhoneName = "";
+            DateReleased = new DateTime();
+        }
+        [ForeignKey("Manufacturer")]
+        public int ManufacturerID { get; set; }
+        [Key]
+        public int PhoneID { get; set; }
+        public String PhoneName { get; set; }
+        public virtual Manufacturer Manufacturer{ get; set; }
+        public int MSRP { get; set; }
+        public int ScreenSize { get; set; }
+        public DateTime DateReleased { get; set; }
+
+    }
+
+
+}
